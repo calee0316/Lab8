97f5634a4bdf5a27c4afe94047ffcc0dd2033ddd
calee0316 <calee@ucsd.edu>
2021-02-24 13:41:10 +0900

    finish

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -7,6 +7,27 @@ describe('Party Horn Tests', () => {
     expect(true).to.equal(true);
   });
 
+  it('Slider changes when volume changes', () => {
+    cy.get('#volume-number').clear().type('75')
+    cy.get('#volume-slider').then($el => {
+      expect($el).to.have.value(75)
+    })
+  })
+
+  it('Number changes when slider volume changes', () => {
+    cy.get('#volume-slider').invoke('val', 33).trigger('input')
+    cy.get('#volume-number').then($el => {
+      expect($el).to.have.value(33)
+    })
+  })
+
+  it('audio volume changes when slider changes', () => {
+    cy.get('#volume-slider').invoke('val', 33).trigger('input')
+    cy.get('#horn-sound').then($el => {
+      expect($el).to.have.prop('volume', 0.33)
+    })
+  })
+
   it('Image & Sound changes when party horn clicked', () => {
     cy.get('#radio-party-horn').click();
     cy.get('#sound-image').then($el => {
@@ -19,13 +40,11 @@ describe('Party Horn Tests', () => {
 
   //volume image changes when increasing volume, for number input
   it('volume image change for mute to level 1, number input', () => {
-    cy.get('#volume-number').invoke('val', 0).trigger('input')
+    cy.get('#volume-number').clear().type('0')
     cy.get('#volume-image').then($el => {
       expect($el).to.have.attr('src', './assets/media/icons/volume-level-0.svg');
     });
-    cy.get('#volume-number')
-      .invoke('val', 1)
-      .trigger('input');
+    cy.get('#volume-number').clear().type('1')
     cy.get('#volume-image').then($el => {
       expect($el).to.have.attr('src', './assets/media/icons/volume-level-1.svg');
     });
@@ -33,26 +52,22 @@ describe('Party Horn Tests', () => {
   
 
   it('volume image change for level 1 to 2, number input', () => {
-    cy.get('#volume-number').invoke('val', 1).trigger('input')
+    cy.get('#volume-number').clear().type('1')
     cy.get('#volume-image').then($el => {
       expect($el).to.have.attr('src', './assets/media/icons/volume-level-1.svg');
     });
-    cy.get('#volume-number')
-      .invoke('val', 34)
-      .trigger('input');
+    cy.get('#volume-number').clear().type('34')
     cy.get('#volume-image').then($el => {
       expect($el).to.have.attr('src', './assets/media/icons/volume-level-2.svg');
     });
   })
   
   it('volume image change for level 2 to level 3, number input', () => {
-    cy.get('#volume-number').invoke('val', 34).trigger('input')
+    cy.get('#volume-number').clear().type('34')
     cy.get('#volume-image').then($el => {
       expect($el).to.have.attr('src', './assets/media/icons/volume-level-2.svg');
     });
-    cy.get('#volume-number')
-      .invoke('val', 67)
-      .trigger('input');
+    cy.get('#volume-number').clear().type('75')
     cy.get('#volume-image').then($el => {
       expect($el).to.have.attr('src', './assets/media/icons/volume-level-3.svg');
     });
